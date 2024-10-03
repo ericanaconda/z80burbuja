58f3b73727c25437c1e1e1bcca03ffdeb5ddcac8
Tristanqsm <136662306+Tristanqsm@users.noreply.github.com>
2024-10-03 00:19:35 -0600

    Factorial.asm

--- Factorial.asm
+++ Factorial.asm
@@ -1,3 +1,5 @@
+; Sanchez Mayen Tristan Qesen
+; Ramirez Valdovinos Eric
 CPU "Z80.tbl"
 HOF "INT8"
 
@@ -8,6 +10,7 @@ HOF "INT8"
       JP z,CasoParticular  ; Verifica si es 0
       CP 1
       JP z,CasoParticular  ; Verifica si es 1
+; Asignamos valores iniciales a los registros
       LD D,A
       LD E,1
       LD B,A
@@ -15,16 +18,16 @@ eti5: LD C,E
       LD A,0
 eti3: DEC C
       JP M,eti2
-      ADD A,B
+      ADD A,B              ; Realiza la multiplicación
       JR eti3
-eti2: INC E                ; 
-      LD B,A
+eti2: INC E                ; Verifica que se hayan multiplicado todos los numeros anteriores
+      LD B,A               ; Salvamos lo acumulado en las multiplicaciones en B
       LD A,E
       CP D
-      JP z,Fin
+      JP z,Fin             ; Si E=D se termino el proceso
       JR eti5
-Fin: LD A,B
+Fin: LD A,B                ; B tiene el factorial del numero
       LD (1001H),A
       HALT
-CasoParticular: LD B,1
+CasoParticular: LD B,1     ; Si el numero es 0 o 1, el factorial es 1
       JR Fin
