cc5b0262ff5f5178031202e1619d724d0f96f427
Tristanqsm <136662306+Tristanqsm@users.noreply.github.com>
2024-10-02 18:14:34 -0600

    Add files via upload

---
+++ Factorial.asm
@@ -0,0 +1,25 @@
+      LD A,(1000H) ; Lee el numero
+      CP 0
+      JP z,CasoParticular  ; Verifica si es 0
+      CP 1
+      JP z,CasoParticular  ; Verifica si es 1
+      LD D,A
+      LD E,1
+      LD B,A
+eti5: LD C,E
+      LD A,0
+eti3: DEC C
+      JP M,eti2
+      ADD A,B
+      JR eti3
+eti2: INC E                ; 
+      LD B,A
+      LD A,E
+      CP D
+      JP z,Fin
+      JR eti5
+Fin: LD A,B
+      LD (1001H),A
+      HALT
+CasoParticular: LD B,1
+      JR Fin
